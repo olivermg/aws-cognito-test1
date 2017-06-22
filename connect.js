c467ce4096c4b658dfc1ef7126c2b71e6f8e6e91
Oliver Wegner <void1976@gmail.com>
2017-06-22 12:05:44 +0200

    adding error handler to connect.js

--- connect.js
+++ connect.js
@@ -4,6 +4,8 @@ var device = awsiot.device({
     clientId: 'iotclient1',
     region: 'eu-west-1',
     protocol: 'wss'
+
+    // via env vars:
     //accessKeyId: process.env['AWS_ACCESS_KEY_ID'],
     //secretKey: process.env['AWS_SECRET_ACCESS_KEY'],
     //sessionToken: process.env['AWS_SESSION_TOKEN']
@@ -20,11 +22,15 @@ device.on('status', function(thing, stat, clientToken, state) {
 device.on('connect', function() {
     console.log('connect');
     device.subscribe('iottopic1', function(res) {
-        console.log('subscribe res:', res);
+        console.log('subscribe res: ', res);
     });
 });
 
 device.on('message', function(topic, payload) {
     console.log('message', topic, payload.toString());
     //device.publish('mytopic2', JSON.stringify({ foo: 'bar123' }));
+});
+
+device.on('error', function(err, args) {
+    console.log('error', err, args);
 });
